a58fcf72d9a7420abebcc4ca4fbad0fcdd77b54b
Badr <32390048+oualib@users.noreply.github.com>
2019-11-27 06:13:52 +0100

    Add files via upload

---
+++ vertica_ml_python/learn/data/__init__.py
@@ -0,0 +1,58 @@
+# (c) Copyright [2018] Micro Focus or one of its affiliates. 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# AUTHOR: BADR OUALI
+#
+############################################################################################################ 
+#  __ __   ___ ____  ______ ____   __  ____      ___ ___ _          ____  __ __ ______ __ __  ___  ____    #
+# |  |  | /  _|    \|      |    | /  ]/    |    |   |   | |        |    \|  |  |      |  |  |/   \|    \   #
+# |  |  |/  [_|  D  |      ||  | /  /|  o  |    | _   _ | |        |  o  |  |  |      |  |  |     |  _  |  #
+# |  |  |    _|    /|_|  |_||  |/  / |     |    |  \_/  | |___     |   _/|  ~  |_|  |_|  _  |  O  |  |  |  #
+# |  :  |   [_|    \  |  |  |  /   \_|  _  |    |   |   |     |    |  |  |___, | |  | |  |  |     |  |  |  #
+#  \   /|     |  .  \ |  |  |  \     |  |  |    |   |   |     |    |  |  |     | |  | |  |  |     |  |  |  #
+#   \_/ |_____|__|\_| |__| |____\____|__|__|    |___|___|_____|    |__|  |____/  |__| |__|__|\___/|__|__|  #
+#                                                                                                          #
+############################################################################################################
+# Vertica-ML-Python allows user to create Virtual Dataframe. vDataframes simplify   #
+# data exploration,   data cleaning   and   machine   learning   in    Vertica.     #
+# It is an object which keeps in it all the actions that the user wants to achieve  # 
+# and execute them when they are needed.    										#
+#																					#
+# The purpose is to bring the logic to the data and not the opposite                #
+#####################################################################################
+#
+__version__ = "1.0"
+__author__ = "Badr Ouali"
+__author_email__ = "badr.ouali@microfocus.com"
+__description__ = """Vertica-ML-Python simplifies data exploration, data cleaning and machine learning in Vertica."""
+__url__ = "https://github.com/vertica/vertica_ml_python/"
+__license__="Apache License, Version 2.0"
+
+# vDataframe
+from vertica_ml_python.vdataframe import vDataframe
+from vertica_ml_python.vdataframe import read_csv
+from vertica_ml_python.vdataframe import read_vdf
+
+
+# Utilities
+from vertica_ml_python.utilities import drop_table
+from vertica_ml_python.utilities import drop_view
+from vertica_ml_python.utilities import drop_model
+from vertica_ml_python.utilities import drop_text_index
+from vertica_ml_python.utilities import tablesample
+from vertica_ml_python.utilities import to_tablesample
+from vertica_ml_python.utilities import read_dsn
+from vertica_ml_python.utilities import vertica_cursor
+
+# Learn
+import vertica_ml_python.learn
