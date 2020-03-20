df9746c557ef3751332a5ee91c7db5d4290b6f47
Badr <32390048+oualib@users.noreply.github.com>
2020-03-20 21:00:12 +0100

    Add files via upload

--- vertica_ml_python/learn/data/__init__.py
+++ vertica_ml_python/learn/data/__init__.py
@@ -30,29 +30,3 @@
 #																					#
 # The purpose is to bring the logic to the data and not the opposite                #
 #####################################################################################
-#
-__version__ = "1.0"
-__author__ = "Badr Ouali"
-__author_email__ = "badr.ouali@microfocus.com"
-__description__ = """Vertica-ML-Python simplifies data exploration, data cleaning and machine learning in Vertica."""
-__url__ = "https://github.com/vertica/vertica_ml_python/"
-__license__="Apache License, Version 2.0"
-
-# vDataframe
-from vertica_ml_python.vdataframe import vDataframe
-from vertica_ml_python.vdataframe import read_csv
-from vertica_ml_python.vdataframe import read_vdf
-
-
-# Utilities
-from vertica_ml_python.utilities import drop_table
-from vertica_ml_python.utilities import drop_view
-from vertica_ml_python.utilities import drop_model
-from vertica_ml_python.utilities import drop_text_index
-from vertica_ml_python.utilities import tablesample
-from vertica_ml_python.utilities import to_tablesample
-from vertica_ml_python.utilities import read_dsn
-from vertica_ml_python.utilities import vertica_cursor
-
-# Learn
-import vertica_ml_python.learn
