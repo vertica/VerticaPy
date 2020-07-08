1a788b9f1d3a783267a0d491611cdc587a03931b
kxu <52899649+ansleis@users.noreply.github.com>
2020-07-08 17:38:47 +0000

    Update tests.php

--- verticamlpython.com/documentation_last/vdataframe/tests.php
+++ verticamlpython.com/documentation_last/vdataframe/tests.php
@@ -7,10 +7,10 @@
         <h2>Statistical Tests</h2>
         <table class="functions_description">
           <tr> <th>Method</th> <th>Definition</th> </tr>
-          <tr> <td>vDataFrame.<a href="main-methods/testdf/index.php">testdf</td> <td>Dickey Fuller test (Time Series stationarity).</td> </tr>
-          <tr> <td>vDataFrame.<a href="main-methods/testdw/index.php">testdw</td> <td>Durbin Watson test (residuals autocorrelation).</td> </tr>
-          <tr> <td>vDataFrame.<a href="main-methods/testjb/index.php">testjb</td> <td>Jarque Bera test (Distribution Normality).</td> </tr>
-          <tr> <td>vDataFrame.<a href="main-methods/testmk/index.php">testmk</td> <td>Mann Kendall test (Time Series trend).</td> </tr>
+          <tr> <td>vDataFrame.<a href="main-methods/testdf/index.php">testdf</td> <td>Dickey-Fuller test (Time Series stationarity).</td> </tr>
+          <tr> <td>vDataFrame.<a href="main-methods/testdw/index.php">testdw</td> <td>Durbin-Watson test (residuals autocorrelation).</td> </tr>
+          <tr> <td>vDataFrame.<a href="main-methods/testjb/index.php">testjb</td> <td>Jarque-Bera test (Distribution Normality).</td> </tr>
+          <tr> <td>vDataFrame.<a href="main-methods/testmk/index.php">testmk</td> <td>Mann-Kendall test (Time Series trend).</td> </tr>
         </table>
       <div><?php include('../../include/footer.php'); ?></div>
       </div>
