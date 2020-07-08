01ff71b5cc900943970ecc97a455d28a8c9d9fb5
kxu <52899649+ansleis@users.noreply.github.com>
2020-07-08 18:30:38 +0000

    Update plot.php

--- verticamlpython.com/documentation_last/learn/plot.php
+++ verticamlpython.com/documentation_last/learn/plot.php
@@ -9,8 +9,8 @@
           <tr> <th>Function</th> <th>Definition</th> </tr>
           <tr> <td><a href="elbow/index.php">elbow</td> <td>Draws the Elbow Curve.</td> </tr>
             <tr> <td><a href="lift_chart/index.php">lift_chart</td> <td>Draws the Lift Chart.</td> </tr>
-          <tr> <td><a href="prc_curve/index.php">prc_curve</td> <td>Draws the PRC Curve.</td> </tr>
-          <tr> <td><a href="roc_curve/index.php">roc_curve</td> <td>Draws the ROC Curve.</td> </tr>
+          <tr> <td><a href="prc_curve/index.php">prc_curve</td> <td>Draws the Precision-recall curve.</td> </tr>
+          <tr> <td><a href="roc_curve/index.php">roc_curve</td> <td>Draws the Rceiver Operating Characteristic (PRC) Curve.</td> </tr>
         </table>
       <div><?php include('../../include/footer.php'); ?></div>
       </div>
