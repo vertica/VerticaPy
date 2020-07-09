62b9fb9ab3fd01a406a2486f7119dc75876110cf
ansleis <52899649+ansleis@users.noreply.github.com>
2020-07-09 11:23:58 -0400

    fix capitalization

--- verticamlpython.com/documentation_last/learn/datasets/index.php
+++ verticamlpython.com/documentation_last/learn/datasets/index.php
@@ -11,7 +11,7 @@
           <tr> <td><a href="load_iris/index.php">load_iris</td> <td>Ingests the Iris dataset in the Vertica DB (Dataset ideal for Classification and Clustering).</td> </tr>
           <tr> <td><a href="load_market/index.php">load_market</td> <td>Ingests the market dataset in the Vertica DB (Dataset ideal for easy exploration).</td> </tr>
           <tr> <td><a href="load_smart_meters/index.php">load_smart_meters</td> <td>Ingests the smart meters dataset in the Vertica DB (Dataset ideal for TS and Regression).</td> </tr>
-          <tr> <td><a href="load_titanic/index.php">load_titanic</td> <td>Ingests the titanic dataset in the Vertica DB (Dataset ideal for Classification).</td> </tr>
+          <tr> <td><a href="load_titanic/index.php">load_titanic</td> <td>Ingests the Titanic dataset in the Vertica DB (Dataset ideal for Classification).</td> </tr>
           <tr> <td><a href="load_winequality/index.php">load_winequality</td> <td>Ingests the winequality dataset in the Vertica DB (Dataset ideal for Regression and Classification).</td> </tr>
         </table>
       <div><?php include('../../../include/footer.php'); ?></div>
