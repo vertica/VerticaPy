91d4899e74612241d16d384dedec091cd157dd28
Arash Fard <jalalzadeh@gmail.com>
2020-09-18 04:11:18 -0400

    Adding the infra-structure for tests

---
+++ verticapy/tests/vDataFrame/test_vDF_filter_sample.py
@@ -0,0 +1,61 @@
+# (c) Copyright [2018-2020] Micro Focus or one of its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from verticapy import vDataFrame
+
+class TestvDFFilterSample():
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_search(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_at_time(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_between_time(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_filter(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_first(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_last(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_drop(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_drop_duplicates(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_drop_outliers(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_select(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_sample(self):
+        pass
---
+++ verticapy/tests/vDataFrame/test_vDF_plot.py
@@ -0,0 +1,73 @@
+# (c) Copyright [2018-2020] Micro Focus or one of its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from verticapy import vDataFrame
+
+class TestvDFPlot():
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_bar(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_boxplot(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_bubble(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_density(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_donut(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_hchart(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_hexbin(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_hist(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_pie(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_pivot_table(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_plot(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_scatter(self):
+        pass
+
+    @pytest.mark.skip(reason="test not implemented")
+    def test_vDF_scatter_matrix(self):
+        pass
