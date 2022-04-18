3f0843a7af11bbb9dbfc9ebb07eb10cbe4445ac2
Badr <badr.ouali@outlook.fr>
2022-04-18 03:43:53 +0200

    Update toolbox.py

--- verticapy/toolbox.py
+++ verticapy/toolbox.py
@@ -856,7 +856,7 @@ def print_table(
 
 # ---#
 def quote_ident(column: str):
-    tmp_column = column
+    tmp_column = str(column)
     if len(column) >= 2 and (column[0] == column[-1] == '"'):
         tmp_column = column[1:-1]
     return '"{}"'.format(str(tmp_column).replace('"', '""'))
