5a6bb7104d1120723a1e696eea17756eb97e18e5
Badr <badr.ouali@outlook.fr>
2023-01-20 13:06:35 -0600

    correction

--- verticapy/utilities.py
+++ verticapy/utilities.py
@@ -121,7 +121,6 @@ feature is available in the user's version.
         fun_name, object_name, condition = func.__name__, "", []
         if len(args) > 0:
             object_name = type(args[0]).__name__
-        all_features_min_versions = 
         name = object_name if fun_name == "__init__" else fun_name
         vertica_version(verticapy.MINIMUM_VERSION[name])
 
