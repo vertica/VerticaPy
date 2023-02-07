bf50f518698d07c995485886594039a1f3bbe508
Badr <badr.ouali@outlook.fr>
2023-02-07 00:02:24 -0600

    correction

--- verticapy/db/execute.py
+++ verticapy/db/execute.py
@@ -14,8 +14,3 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from verticapy.io import *
-from verticapy._config.config import *
-from verticapy.db.create import *
-from verticapy.db.drop import *
-from verticapy._help import *
