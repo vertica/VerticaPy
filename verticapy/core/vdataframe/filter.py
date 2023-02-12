189b2efae87233a8e6588522e885b4f9446ae607
Badr <badr.ouali@outlook.fr>
2023-02-12 16:27:16 -0600

    Update filter.py

--- verticapy/core/vdataframe/filter.py
+++ verticapy/core/vdataframe/filter.py
@@ -14,7 +14,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-import random, warnings
+import random, warnings, datetime
 from typing import Union, Literal
 from collections.abc import Iterable
 from verticapy.errors import ParameterError
