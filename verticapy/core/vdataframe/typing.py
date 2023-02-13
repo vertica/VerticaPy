c222b1b06a476f211151377b14e2bf0e50047c58
Badr <badr.ouali@outlook.fr>
2023-02-13 15:10:31 -0600

    Update typing.py

--- verticapy/core/vdataframe/typing.py
+++ verticapy/core/vdataframe/typing.py
@@ -14,6 +14,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
+from typing import Union
 from verticapy._utils._collect import save_verticapy_logs
 from verticapy._utils._sql import _executeSQL
 from verticapy.core.tablesample import tablesample
