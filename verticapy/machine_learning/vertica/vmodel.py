12da0f02e2587d6b6456d887c20cbf2ab3a23b66
Badr <badr.ouali@outlook.fr>
2023-02-15 12:11:41 -0600

    correction

--- verticapy/machine_learning/vertica/vmodel.py
+++ verticapy/machine_learning/vertica/vmodel.py
@@ -48,7 +48,7 @@ from verticapy.learn.tools import does_model_exist
 from verticapy.sql._utils._format import clean_query, quote_ident, schema_relation
 from verticapy.machine_learning._utils import get_match_index
 from verticapy._config.config import OPTIONS
-import verticapy.machine_learning.metrics.metrics as mt
+import verticapy.machine_learning.metrics as mt
 import verticapy.machine_learning.model_selection as ms
 
 ##
