4c4651eedd60cba73799fc8cefa8d5557de4d215
Badr <badr.ouali@outlook.fr>
2023-02-15 19:06:21 -0600

    correction

--- verticapy/__init__.py
+++ verticapy/__init__.py
@@ -97,4 +97,9 @@ from verticapy.sql.parsers.csv import read_csv
 from verticapy.sql.parsers.json import read_json
 from verticapy.sql.parsers.pandas import pandas_to_vertica
 from verticapy.sql.parsers.shp import read_shp
+from verticapy.sql.create import create_schema, create_table, create_verticapy_schema
 from verticapy.sql.drop import drop
+from verticapy.sql.dtypes import get_data_types
+from verticapy.sql.insert import insert_into
+from verticapy.sql.read import readSQL, to_tablesample, vDataFrameSQL
+from verticapy.sql.sys import current_session, username
