fcb094e30cf819d9ed6dbb9734d31e2def1b04ad
Badr <badr.ouali@outlook.fr>
2023-02-17 10:06:28 -0600

    Update test_model_selection.py

--- verticapy/tests/vModel/test_model_selection.py
+++ verticapy/tests/vModel/test_model_selection.py
@@ -22,7 +22,8 @@ import pytest
 import matplotlib.pyplot as plt
 
 # VerticaPy
-import verticapy, drop
+import verticapy
+from verticapy import drop
 from verticapy.core.vdataframe.base import vDataFrame
 from verticapy._config.config import set_option
 from verticapy.connect import current_cursor
