b0d66d6262154f6576771556de3205874cb8ff01
Badr <badr.ouali@outlook.fr>
2023-02-24 10:40:24 -0600

    correction

--- verticapy/core/string_sql/base.py
+++ verticapy/core/string_sql/base.py
@@ -14,7 +14,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from typing import Iterable
+from typing import Iterable, Literal
 
 from verticapy._utils._sql._format import format_magic
 from verticapy.errors import ParameterError
--- verticapy/core/tablesample/base.py
+++ verticapy/core/tablesample/base.py
@@ -15,6 +15,7 @@ See the  License for the specific  language governing
 permissions and limitations under the License.
 """
 import copy, decimal, datetime, math
+from typing import Literal
 import numpy as np
 
 import pandas as pd
