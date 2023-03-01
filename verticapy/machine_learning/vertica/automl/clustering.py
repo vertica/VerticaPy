64b6b6cc71ae0b6c02392602057f6c058eee37c3
Badr <badr.ouali@outlook.fr>
2023-03-01 15:07:31 -0600

    Update clustering.py

--- verticapy/machine_learning/vertica/automl/clustering.py
+++ verticapy/machine_learning/vertica/automl/clustering.py
@@ -14,7 +14,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from typing import Union
+from typing import Literal, Union
 from tqdm.auto import tqdm
 
 import verticapy._config.config as conf
