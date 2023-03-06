1bcc95e2eb5ce0b6e8092250204e6be7659afcc2
Badr <badr.ouali@outlook.fr>
2023-03-05 23:43:49 -0600

    Code Indent + Type Corrections.

--- verticapy/core/string_sql/base.py
+++ verticapy/core/string_sql/base.py
@@ -14,35 +14,41 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from typing import Iterable, Literal
+from typing import Any, Iterable, Literal
 
+from verticapy._typing import SQLColumns
 from verticapy._utils._sql._format import format_magic
 from verticapy.errors import ParameterError
 
 
 class StringSQL:
+    """
+    Class used to represent SQL strings.
+    """
+
     @property
     def _object_type(self) -> Literal["StringSQL"]:
         return "StringSQL"
 
-    def __init__(self, alias, category="", init_transf=""):
+    def __init__(self, alias, category: str = "", init_transf: str = "") -> None:
         self._alias = alias
-        self.category_ = category
+        self._category = category
         if not (init_transf):
             self._init_transf = alias
         else:
             self._init_transf = init_transf
+        return None
 
-    def __repr__(self):
+    def __repr__(self) -> "StringSQL":
         return str(self._init_transf)
 
-    def __str__(self):
+    def __str__(self) -> "StringSQL":
         return str(self._init_transf)
 
-    def __abs__(self):
+    def __abs__(self) -> "StringSQL":
         return StringSQL(f"ABS({self._init_transf})", self.category())
 
-    def __add__(self, x):
+    def __add__(self, x: Any) -> "StringSQL":
         if (hasattr(self, "isarray") and self.isarray()) and (
             hasattr(x, "isarray") and x.isarray()
         ):
@@ -57,7 +63,7 @@ class StringSQL:
         )
         return StringSQL(f"({self._init_transf}) {op} ({val})", self.category())
 
-    def __radd__(self, x):
+    def __radd__(self, x: Any) -> "StringSQL":
         if (hasattr(self, "isarray") and self.isarray()) and (
             hasattr(x, "isarray") and x.isarray()
         ):
@@ -72,22 +78,22 @@ class StringSQL:
         )
         return StringSQL(f"({val}) {op} ({self._init_transf})", self.category())
 
-    def __and__(self, x):
+    def __and__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) AND ({val})", self.category())
 
-    def __rand__(self, x):
+    def __rand__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({val}) AND ({self._init_transf})", self.category())
 
-    def _between(self, x, y):
+    def _between(self, x: Any, y: Any) -> "StringSQL":
         val1 = str(format_magic(x))
         val2 = str(format_magic(y))
         return StringSQL(
             f"({self._init_transf}) BETWEEN ({val1}) AND ({val2})", self.category(),
         )
 
-    def _in(self, *argv):
+    def _in(self, *argv) -> "StringSQL":
         if (len(argv) == 1) and (isinstance(argv[0], list)):
             x = argv[0]
         elif len(argv) == 0:
@@ -101,7 +107,7 @@ class StringSQL:
         val = ", ".join(val)
         return StringSQL(f"({self._init_transf}) IN ({val})", self.category())
 
-    def _not_in(self, *argv):
+    def _not_in(self, *argv) -> "StringSQL":
         if (len(argv) == 1) and (isinstance(argv[0], list)):
             x = argv[0]
         elif len(argv) == 0:
@@ -117,13 +123,13 @@ class StringSQL:
         val = ", ".join(val)
         return StringSQL(f"({self._init_transf}) NOT IN ({val})", self.category())
 
-    def _as(self, x):
+    def _as(self, x: Any) -> "StringSQL":
         return StringSQL(f"({self._init_transf}) AS {x}", self.category())
 
-    def _distinct(self):
+    def _distinct(self) -> "StringSQL":
         return StringSQL(f"DISTINCT ({self._init_transf})", self.category())
 
-    def _over(self, by: (str, list) = [], order_by: (str, list) = []):
+    def _over(self, by: SQLColumns = [], order_by: SQLColumns = []):
         if isinstance(by, str):
             by = [by]
         if isinstance(order_by, str):
@@ -138,116 +144,116 @@ class StringSQL:
             f"{self._init_transf} OVER ({by} {order_by})", self.category(),
         )
 
-    def __eq__(self, x):
+    def __eq__(self, x: Any) -> "StringSQL":
         op = "IS" if (x == None) and not (isinstance(x, StringSQL)) else "="
         val = format_magic(x)
         if val != "NULL":
             val = f"({val})"
         return StringSQL(f"({self._init_transf}) {op} {val}", self.category())
 
-    def __ne__(self, x):
+    def __ne__(self, x: Any) -> "StringSQL":
         op = "IS NOT" if (x == None) and not (isinstance(x, StringSQL)) else "!="
         val = format_magic(x)
         if val != "NULL":
             val = f"({val})"
         return StringSQL(f"({self._init_transf}) {op} {val}", self.category())
 
-    def __ge__(self, x):
+    def __ge__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) >= ({val})", self.category())
 
-    def __gt__(self, x):
+    def __gt__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) > ({val})", self.category())
 
-    def __le__(self, x):
+    def __le__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) <= ({val})", self.category())
 
-    def __lt__(self, x):
+    def __lt__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) < ({val})", self.category())
 
-    def __mul__(self, x):
+    def __mul__(self, x: Any) -> "StringSQL":
         if self.category() == "text" and isinstance(x, int):
             return StringSQL(f"REPEAT({self._init_transf}, {x})", self.category())
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) * ({val})", self.category())
 
-    def __rmul__(self, x):
+    def __rmul__(self, x: Any) -> "StringSQL":
         if self.category() == "text" and isinstance(x, int):
             return StringSQL(f"REPEAT({self._init_transf}, {x})", self.category())
         val = format_magic(x)
         return StringSQL(f"({val}) * ({self._init_transf})", self.category())
 
-    def __or__(self, x):
+    def __or__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) OR ({val})", self.category())
 
-    def __ror__(self, x):
+    def __ror__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({val}) OR ({self._init_transf})", self.category())
 
-    def __pos__(self):
+    def __pos__(self) -> "StringSQL":
         return StringSQL(f"+({self._init_transf})", self.category())
 
-    def __neg__(self):
+    def __neg__(self) -> "StringSQL":
         return StringSQL(f"-({self._init_transf})", self.category())
 
-    def __pow__(self, x):
+    def __pow__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"POWER({self._init_transf}, {val})", self.category())
 
-    def __rpow__(self, x):
+    def __rpow__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"POWER({val}, {self._init_transf})", self.category())
 
-    def __mod__(self, x):
+    def __mod__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"MOD({self._init_transf}, {val})", self.category())
 
-    def __rmod__(self, x):
+    def __rmod__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"MOD({val}, {self._init_transf})", self.category())
 
-    def __sub__(self, x):
+    def __sub__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) - ({val})", self.category())
 
-    def __rsub__(self, x):
+    def __rsub__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({val}) - ({self._init_transf})", self.category())
 
-    def __truediv__(self, x):
+    def __truediv__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) / ({val})", self.category())
 
-    def __rtruediv__(self, x):
+    def __rtruediv__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({val}) / ({self._init_transf})", self.category())
 
-    def __floordiv__(self, x):
+    def __floordiv__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({self._init_transf}) // ({val})", self.category())
 
-    def __rfloordiv__(self, x):
+    def __rfloordiv__(self, x: Any) -> "StringSQL":
         val = format_magic(x)
         return StringSQL(f"({val}) // ({self._init_transf})", self.category())
 
-    def __ceil__(self):
+    def __ceil__(self) -> "StringSQL":
         return StringSQL(f"CEIL({self._init_transf})", self.category())
 
-    def __floor__(self):
+    def __floor__(self) -> "StringSQL":
         return StringSQL(f"FLOOR({self._init_transf})", self.category())
 
-    def __trunc__(self):
+    def __trunc__(self) -> "StringSQL":
         return StringSQL(f"TRUNC({self._init_transf})", self.category())
 
-    def __invert__(self):
+    def __invert__(self) -> "StringSQL":
         return StringSQL(f"-({self._init_transf}) - 1", self.category())
 
-    def __round__(self, x):
+    def __round__(self, x) -> "StringSQL":
         return StringSQL(f"ROUND({self._init_transf}, {x})", self.category())
 
-    def category(self):
-        return self.category_
+    def category(self) -> str:
+        return self._category
