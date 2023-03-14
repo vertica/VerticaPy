7684771bb1fd5c04c4fa6c963ed76cf7cf7d9219
umar <46414488+mail4umar@users.noreply.github.com>
2023-03-14 13:23:08 -0500

    Added separate file for plotly tests
    Added a separate file and incorporated tests for bar chart.
    
    Removed the tests from the original test_vDF_plot file.

--- verticapy/tests/vDataFrame/test_vDF_plot.py
+++ verticapy/tests/vDataFrame/test_vDF_plot.py
@@ -24,7 +24,6 @@ import datetime, os, sys
 # Other Modules
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import plotly.express as px
 from vertica_highcharts.highcharts.highcharts import Highchart
 from vertica_highcharts.highstock.highstock import Highstock
 
@@ -613,25 +612,6 @@ class TestvDFPlot:
             1
         ].get_height() == pytest.approx(0.4327390599675851)
         plt.close("all")
-
-        # for plotly
-        ## 1D bar charts
-        conf.set_option("plotting_lib","plotly")
-        survived_values=titanic_vd.to_pandas()["survived"]
-        test_fig=px.bar(
-            x=[0,1], 
-            y=[survived_values[survived_values==0].count(),survived_values[survived_values==1].count()]
-            )
-        test_fig=test_fig.update_xaxes(type='category')
-        result=titanic_vd["survived"].hist()
-        assert(test_fig.data[0]['y'][0]/test_fig.data[0]['y'][1]==result.data[0]['y'][0]/result.data[0]['y'][1])
-        assert(test_fig.data[0]['x'][0]==result.data[0]['x'][0])
-        assert(test_fig.layout['xaxis']['type']=='category')
-        result=titanic_vd["survived"].hist(xaxis_title="Custom X Axis Title")
-        assert(result.layout['xaxis']['title']['text']=='Custom X Axis Title')
-        result=titanic_vd["survived"].hist(yaxis_title="Custom Y Axis Title")
-        assert(result.layout['yaxis']['title']['text']=='Custom Y Axis Title')
-        conf.set_option("plotting_lib","matplotlib")
 
     @pytest.mark.skipif(
         sys.version_info >= (3, 7),
---
+++ verticapy/tests/vDataFrame/test_vDF_plot_plotly.py
@@ -0,0 +1,116 @@
+"""
+(c)  Copyright  [2018-2023]  OpenText  or one of its
+affiliates.  Licensed  under  the   Apache  License,
+Version 2.0 (the  "License"); You  may  not use this
+file except in compliance with the License.
+
+You may obtain a copy of the License at:
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless  required  by applicable  law or  agreed to in
+writing, software  distributed  under the  License is
+distributed on an  "AS IS" BASIS,  WITHOUT WARRANTIES
+OR CONDITIONS OF ANY KIND, either express or implied.
+See the  License for the specific  language governing
+permissions and limitations under the License.
+"""
+
+# Pytest
+import pytest
+
+# Standard Python Modules
+import datetime, os, sys
+
+# Other Modules
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import plotly.express as px
+from vertica_highcharts.highcharts.highcharts import Highchart
+from vertica_highcharts.highstock.highstock import Highstock
+
+# VerticaPy
+import verticapy
+import verticapy._config.config as conf
+from verticapy import drop, set_option
+from verticapy.datasets import (
+    load_titanic,
+    load_amazon,
+    load_commodities,
+    load_iris,
+    load_world,
+    load_pop_growth,
+    load_gapminder,
+)
+
+set_option("print_info", False)
+conf.set_option("plotting_lib","plotly")
+
+@pytest.fixture(scope="module")
+def titanic_vd():
+    titanic = load_titanic()
+    yield titanic
+    drop(name="public.titanic")
+
+
+@pytest.fixture(scope="module")
+def amazon_vd():
+    amazon = load_amazon()
+    yield amazon
+    drop(name="public.amazon")
+
+
+@pytest.fixture(scope="module")
+def commodities_vd():
+    commodities = load_commodities()
+    yield commodities
+    drop(name="public.commodities")
+
+
+@pytest.fixture(scope="module")
+def iris_vd():
+    iris = load_iris()
+    yield iris
+    drop(name="public.iris")
+
+
+@pytest.fixture(scope="module")
+def world_vd():
+    cities = load_world()
+    yield cities
+    drop(name="public.world")
+
+
+@pytest.fixture(scope="module")
+def pop_growth_vd():
+    pop_growth = load_pop_growth()
+    yield pop_growth
+    drop(name="public.pop_growth")
+
+
+@pytest.fixture(scope="module")
+def gapminder_vd():
+    gapminder = load_gapminder()
+    yield gapminder
+    drop(name="public.gapminder")
+
+
+class TestvDFPlot:
+    
+    def test_vDF_hist(self, titanic_vd):
+        # for plotly
+        ## 1D bar charts
+
+        survived_values=titanic_vd.to_pandas()["survived"]
+        test_fig=px.bar(
+            x=[0,1], 
+            y=[survived_values[survived_values==0].count(),survived_values[survived_values==1].count()]
+            )
+        test_fig=test_fig.update_xaxes(type='category')
+        result=titanic_vd["survived"].hist()
+        assert(test_fig.data[0]['y'][0]/test_fig.data[0]['y'][1]==result.data[0]['y'][0]/result.data[0]['y'][1])
+        assert(test_fig.data[0]['x'][0]==result.data[0]['x'][0])
+        assert(test_fig.layout['xaxis']['type']=='category')
+        result=titanic_vd["survived"].hist(xaxis_title="Custom X Axis Title")
+        assert(result.layout['xaxis']['title']['text']=='Custom X Axis Title')
+        result=titanic_vd["survived"].hist(yaxis_title="Custom Y Axis Title")
+        assert(result.layout['yaxis']['title']['text']=='Custom Y Axis Title')
