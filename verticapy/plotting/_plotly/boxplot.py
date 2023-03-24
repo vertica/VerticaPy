770a958a105cf7f3e946061e2d324fc7eb93d8f0
umar <46414488+mail4umar@users.noreply.github.com>
2023-03-24 11:10:12 -0500

    Update boxplot.py
    - added option for multiple box plots

--- verticapy/plotting/_plotly/boxplot.py
+++ verticapy/plotting/_plotly/boxplot.py
@@ -16,7 +16,9 @@ permissions and limitations under the License.
 """
 from typing import Literal
 
+import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 from plotly.graph_objs._figure import Figure
 
 from verticapy.plotting._plotly.base import PlotlyBase
@@ -44,31 +46,102 @@ class BoxPlot(PlotlyBase):
         self.init_layout_style = {}
         return None
 
+    def _create_bar_info(self,y_value,base,bar,labl_value,labl) -> Figure:
+        data=[[y_value,bar-base,base]]
+        df=pd.DataFrame(data, columns=['Y', 'bar',"base"])
+        fig=px.bar(df,y="Y",x="bar",base="base",orientation="h",barmode="stack",
+                ).update_traces(opacity=0.05, hovertemplate=f'{labl}:{labl_value}')
+        return fig
+
+    def _create_dataframe_for_outliers(self,fliers,traces):
+        a=[]
+        b=[]
+        c=[]
+        for i,ele in enumerate(fliers):
+            for j in range(len(ele)):
+                if ele[j]:
+                    a.append(ele[j])
+                    b.append(traces[i])
+                    c.append(self.get_colors()[i])
+        df = pd.DataFrame({
+            'value': a,
+            'trace': b,
+            'color': c
+        })
+        return df
+    
     # Draw.
 
     def draw(self, **style_kwargs) -> Figure:
         """
         Draws a boxplot using the Plotly API.
         """
-        fig = go.Figure()
-        fig.add_trace(go.Box(
-            x=self.data['fliers'], 
-            boxpoints='outliers',
-            hovertemplate ='%{x}',
-                        ),**style_kwargs)
-        fig.update_traces(q1=self.data['X'][1], 
-                        median=self.data['X'][2],
-                        q3=self.data['X'][3], 
-                        lowerfence=self.data['X'][0],
-                        upperfence=self.data['X'][4],
-                        )
-        fig.update_layout(
-            yaxis = dict(
-                showticklabels=False,
-                title=self.layout['labels'][0][1:-1]
+        print("SHAPE OF X IS:",self.data['X'].shape)
+        if self.data['X'].shape[1]<2:
+            print("ONE DIMENSIONAL")
+            min_val=self.data['X'][0][0]
+            q1=self.data['X'][1][0]
+            median=self.data['X'][2][0]
+            q3=self.data['X'][3][0]
+            max_val=self.data['X'][4][0]
+            fig = go.Figure()
+            fig.add_trace(go.Box(
+                x=self.data['fliers'], 
+                boxpoints='outliers',
+                hovertemplate ='%{x}',
+                            ),**style_kwargs)
+            fig.update_traces(q1=[q1], 
+                            median=[median],
+                            q3=[q3], 
+                            lowerfence=[min_val],
+                            upperfence=[max_val],
+                            )
+            fig.update_layout(
+                yaxis = dict(
+                    showticklabels=False,
+                    title=self.layout['labels'][0]
+                )
             )
-        )
-        fig.update_layout(hovermode='y')
+            bins=[min_val,(min_val+q1),(q1+median)/2,(median+q3)/2,(q3+max_val)/2,max_val]
+            labels=["Min",f"{self.data['q'][0]*100}%","Median",f"{self.data['q'][1]*100}% ","Maximum"]
+            values=[min_val,q1,median,q3,max_val]
+            for i in range(len(values)):        
+                fig_add=self._create_bar_info(0,bins[i],bins[i+1],values[i],labels[i])
+                fig.add_traces(fig_add.data)
+            fig.update_layout(barmode='relative')
+        else:
+            fig = go.Figure()
+            for I in range(self.data['X'].shape[1]):
+                I=[I]
+                fig.add_trace(go.Box(
+                    x=([self.layout['labels'][I[0]]]),
+                    #y=D['fliers'][I[0]], 
+                    #y=data,
+                    name=self.layout['labels'][I[0]],
+                    #boxpoints='outliers',
+                        hovertemplate ='%{x}',
+                #    hover_data=['year','Mean','Median','IQR'],
+                                ))
+                fig.update_traces(q1=self.data['X'][1,I], 
+                                median=self.data['X'][2,I],
+                                q3=self.data['X'][3,I], 
+                                lowerfence=self.data['X'][0,I],
+                                upperfence=self.data['X'][4,I],
+                                orientation='v',
+                                selector = ({'name':self.layout['labels'][I[0]]}))
+            df=self._create_dataframe_for_outliers(self.data['fliers'],self.layout['labels'])
+            fig_1 = px.strip(df,
+                x='trace',
+                y='value',
+                color='color',
+                stripmode='overlay',
+                            hover_data ={**{"color": False, "trace": False}},
+                            )
+            fig_1.update_layout(showlegend=False)
+            for i in range(len(fig_1.data)):
+                
+                fig.add_trace(fig_1.data[i])
+            fig.update_layout(showlegend=False)
         return fig
 
 
