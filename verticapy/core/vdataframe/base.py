d113554413134dd1811a504701964669929bc7dd
Badr <badr.ouali@outlook.fr>
2023-04-02 19:46:37 -0500

    Update base.py

--- verticapy/core/vdataframe/base.py
+++ verticapy/core/vdataframe/base.py
@@ -15,7 +15,7 @@ See the  License for the specific  language governing
 permissions and limitations under the License.
 """
 import copy, warnings
-from typing import Literal, Union
+from typing import Literal, Optional, Union
 import numpy as np
 
 import pandas as pd
