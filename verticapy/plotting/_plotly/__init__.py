c864ba0322922378658ee01b20fb02e23d8ad451
Umar Farooq Ghumman <46414488+mail4umar@users.noreply.github.com>
2023-04-04 15:34:03 -0500

    PLOTTING: Plotly - Multi Lineplot. (#542)
    * plotly integration - range curve
    
    - range curve
    - unit tests
    
    * testing bugs fixed
    
    - fixed test bugs
    
    * Update _plotting.py
    
    * Update test_vDF_plot_plotly.py
    
    * Update line.py
    
    * fixed bugs + unit tests added
    
    * Update __init__.py
    
    * Update test_vDF_plot_plotly.py
    
    - fix test bug

--- verticapy/plotting/_plotly/__init__.py
+++ verticapy/plotting/_plotly/__init__.py
@@ -24,6 +24,7 @@ from verticapy.plotting._plotly.contour_plot import ContourPlot
 from verticapy.plotting._plotly.density import DensityPlot, MultiDensityPlot
 from verticapy.plotting._plotly.spider import SpiderChart
 from verticapy.plotting._plotly.range import RangeCurve
+from verticapy.plotting._plotly.line import LinePlot, MultiLinePlot
 from verticapy.plotting._plotly.base import PlotlyBase
 
 import plotly.io as pio
