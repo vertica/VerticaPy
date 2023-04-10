b8a1bd09c0910935549d32b3c33c0881422a1490
Badr <badr.ouali@outlook.fr>
2023-04-10 17:21:10 +0200

    Update csv.py

--- verticapy/core/parsers/csv.py
+++ verticapy/core/parsers/csv.py
@@ -328,7 +328,7 @@ def read_csv(
     path = path.replace("'", "''")
     sep = sep.replace("'", "''")
     header_names = [str(elem).replace("'", "''") for elem in header_names]
-    na_rep = na_rep.replace("'", "''") if isinstance(na_rep, NoneType) else ""
+    na_rep = "" if isinstance(na_rep, NoneType) else na_rep.replace("'", "''")
     quotechar = quotechar.replace("'", "''")
     escape = escape.replace("'", "''")
     file_extension = path.split(".")[-1].lower()
