f9dee1044f0458a3511ad9900d71204efe0ad699
Umar Farooq Ghumman <46414488+mail4umar@users.noreply.github.com>
2023-04-12 13:37:00 -0500

    Plotly ml logistic reg (#566)
    * plotly integration
    
    logistic reg draft
    
    * Update logistic_reg.py
    
    - added 3d plot
    
    * updated for 3d
    
    - update logistic regression code
    - added unit tests
    - also re-added LOF plot

--- verticapy/plotting/_plotly/__init__.py
+++ verticapy/plotting/_plotly/__init__.py
@@ -33,6 +33,10 @@ from verticapy.plotting._plotly.base import PlotlyBase
 from verticapy.plotting._plotly.machine_learning.regression import RegressionPlot
 from verticapy.plotting._plotly.machine_learning.elbow import ElbowCurve
 from verticapy.plotting._plotly.machine_learning.lof import LOFPlot
+from verticapy.plotting._plotly.machine_learning.logistic_reg import (
+    LogisticRegressionPlot,
+)
+
 
 
 import plotly.io as pio
