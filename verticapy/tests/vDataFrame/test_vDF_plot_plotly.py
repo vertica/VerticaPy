25323cd17ac52168b09d4c5d07c3bf1ae9761470
Umar Farooq Ghumman <46414488+mail4umar@users.noreply.github.com>
2023-04-20 12:26:49 -0500

    plotly integration - stepwise (#587)
    - stepwise plot
    - unit test

--- verticapy/tests/vDataFrame/test_vDF_plot_plotly.py
+++ verticapy/tests/vDataFrame/test_vDF_plot_plotly.py
@@ -47,6 +47,7 @@ from verticapy.learn.decomposition import PCA
 from verticapy.learn.svm import LinearSVC
 from verticapy.learn.tree import DecisionTreeRegressor
 from verticapy.learn.delphi import AutoML
+from verticapy.learn.model_selection import stepwise
 
 conf.set_option("print_info", False)
 
@@ -281,11 +282,35 @@ def svm_3d_plot_result(load_plotly, iris_one_hot_vd):
 
 @pytest.fixture(scope="class")
 def champion_challenger_plot_result(load_plotly, titanic_vd):
-    model = AutoML("model_automl",
-                lmax = 10,
-                print_info = False)
-    model.fit(titanic_vd, ["age",], "survived",)
+    model = AutoML("model_automl", lmax=10, print_info=False)
+    model.fit(
+        titanic_vd,
+        [
+            "age",
+        ],
+        "survived",
+    )
     return model.plot()
+
+
+@pytest.fixture(scope="class")
+def stepwise_plot_result(load_plotly, titanic_vd):
+    model = LogisticRegression(
+        name="test_LR_titanic", tol=1e-4, max_iter=100, solver="Newton"
+    )
+    stepwise_result = stepwise(
+        model,
+        input_relation=titanic_vd,
+        X=[
+            "age",
+            "fare",
+            "parch",
+            "pclass",
+        ],
+        y="survived",
+        direction="backward",
+    )
+    return stepwise_result.step_wise_
 
 
 @pytest.fixture(scope="module")
@@ -2525,3 +2550,85 @@ class TestMachineLearningChampionChallengerPlot:
         assert (
             type(self.result) == plotly.graph_objs._figure.Figure
         ), "Wrong object crated"
+
+
+class TestMachineLearningStepwisePlot:
+    @pytest.fixture(autouse=True)
+    def result(self, stepwise_plot_result):
+        self.result = stepwise_plot_result
+
+    def test_properties_output_type(self):
+        # Arrange
+        # Act
+        # Assert - checking if correct object created
+        assert (
+            type(self.result) == plotly.graph_objs._figure.Figure
+        ), "Wrong object crated"
+
+    def test_properties_xaxis_label(self):
+        # Arrange
+        test_title = "n_features"
+        # Act
+        # Assert
+        assert (
+            self.result.layout["xaxis"]["title"]["text"] == test_title
+        ), "X axis label incorrect"
+
+    def test_properties_yaxis_label(self):
+        # Arrange
+        test_title = "bic"
+        # Act
+        # Assert
+        assert (
+            self.result.layout["yaxis"]["title"]["text"] == test_title
+        ), "Y axis label incorrect"
+
+    def test_properties_no_of_elements(self):
+        # Arrange
+        total_items = 8
+        # Act
+        # Assert
+        assert len(self.result.data) == pytest.approx(
+            total_items, abs=1
+        ), "Some elements missing"
+
+    def test_data_start_and_end(self):
+        # Arrange
+        start = "Start"
+        end = "End"
+        # Act
+        # Assert
+        assert start in [
+            self.result.data[i]["name"] for i in range(len(self.result.data))
+        ] and end in [
+            self.result.data[i]["name"] for i in range(len(self.result.data))
+        ], "Some elements missing"
+
+    def test_additional_options_custom_height(self, load_plotly, titanic_vd):
+        # rrange
+        custom_height = 650
+        custom_width = 700
+        model = LogisticRegression(
+            name="test_LR_titanic", tol=1e-4, max_iter=100, solver="Newton"
+        )
+        # Act
+        stepwise_result = stepwise(
+            model,
+            input_relation=titanic_vd,
+            X=[
+                "age",
+                "fare",
+                "parch",
+                "pclass",
+            ],
+            y="survived",
+            direction="backward",
+            height=custom_height,
+            width=custom_width,
+        )
+        result = stepwise_result.step_wise_
+        # Assert
+        assert (
+            result.layout["height"] == custom_height
+            and result.layout["width"] == custom_width
+        ), "Custom height and width not working"
