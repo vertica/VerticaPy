8475e2ee06146c2fd51c23d65468cc59c8b235a2
mail4umar <umarfarooq.ghumman@microfocus.com>
2023-05-11 08:47:42 -0500

    corrected import

--- verticapy/tests_new/plotting/matplotlib/test_acf.py
+++ verticapy/tests_new/plotting/matplotlib/test_acf.py
@@ -8,7 +8,7 @@ import pytest
 import numpy as np
 
 # Vertica
-from verticapy.tests_new.conftest import get_xaxis_label, get_yaxis_label
+from verticapy.tests_new.plotting.conftest import get_xaxis_label, get_yaxis_label
 
 
 @pytest.fixture(scope="class")
