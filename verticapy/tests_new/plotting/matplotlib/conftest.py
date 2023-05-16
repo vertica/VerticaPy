0426831cb80c48a4e5a40edd3a0bd4b8e8131f92
mail4umar <umarfarooq.ghumman@microfocus.com>
2023-05-16 09:08:36 -0500

    Update conftest.py

--- verticapy/tests_new/plotting/matplotlib/conftest.py
+++ verticapy/tests_new/plotting/matplotlib/conftest.py
@@ -26,12 +26,10 @@ import matplotlib.pyplot as plt
 # VerticaPy
 import verticapy._config.config as conf
 
-DUMMY_TEST_SIZE = 100
-
 
 @pytest.fixture(scope="module")
 def plotting_library_object(matplotlib_figure_object):
-    return plt.Axes
+    return matplotlib_figure_object
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -39,3 +37,8 @@ def load_matplotlib():
     conf.set_option("plotting_lib", "matplotlib")
     yield
     conf.set_option("plotting_lib", "matplotlib")
+
+
+@pytest.fixture(scope="session")
+def matplotlib_figure_object():
+    yield plt.Axes
