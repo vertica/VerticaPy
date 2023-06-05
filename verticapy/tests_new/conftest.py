7d87b2714000d1067d42f990ffc37e64af80a4e9
Vikash Singh <29343439+vikash018@users.noreply.github.com>
2023-06-05 13:50:10 -0400

    added unix space in conftest.py file to match master to reslove conflict

--- verticapy/tests_new/conftest.py
+++ verticapy/tests_new/conftest.py
@@ -13,7 +13,7 @@ distributed on an  "AS IS" BASIS,  WITHOUT WARRANTIES
 OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
-"""
+"""  
 # Standard Python Modules
 import random
 import tempfile
