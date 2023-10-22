9bc769ffee7330ef263815f4438f0fbddb8b281e
umar <46414488+mail4umar@users.noreply.github.com>
2023-10-22 05:15:26 -0500

    Update config.py

--- verticapy/_config/config.py
+++ verticapy/_config/config.py
@@ -386,14 +386,23 @@ def set_option(key: str, value: Any = None) -> None:
 
     .. important:: The API will exclusively use these colors for drawing graphics.
 
-    .. code-block:: python
+    .. ipython:: python
 
         set_option("colors", ["blue", "red"])
+
+        @savefig _config_config_set_option_hist.png
+        titanic.hist(["pclass", "survived"])
 
     .. warning::
 
         This can be unstable if not enough colors are provided. It is advised to
         use the plotting library color options to switch colors.
+
+    .. ipython:: python
+        :suppress:
+
+        set_option("colors", None)
+
 
     Utilities
     =========
