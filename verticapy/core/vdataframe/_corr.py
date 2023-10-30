d985c857f02e10e6abc1957b47e98503b1e1a7f9
Badr <badr.ouali@outlook.fr>
2023-10-29 20:46:55 -0400

    test correction

--- verticapy/core/vdataframe/_corr.py
+++ verticapy/core/vdataframe/_corr.py
@@ -233,7 +233,10 @@ class vDFCorr(vDFEncode):
                 phi2corr = max(0, phi2 - ((k - 1) * (r - 1)) / (n - 1))
                 rcorr = r - ((r - 1) ** 2) / (n - 1)
                 kcorr = k - ((k - 1) ** 2) / (n - 1)
-                return np.sqrt(phi2corr / min((kcorr - 1), (rcorr - 1)))
+                div = min((kcorr - 1), (rcorr - 1))
+                if div == 0:
+                    return np.nan
+                return np.sqrt(phi2corr / div)
             elif method == "kendall":
                 if columns[1] == columns[0]:
                     return 1
