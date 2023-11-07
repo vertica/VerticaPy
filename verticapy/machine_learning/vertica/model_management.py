7c6a022e22f4d666fffc3ad461283e15b8eddb68
Badr <badr.ouali@outlook.fr>
2023-11-06 22:28:58 -0500

    Update model_management.py

--- verticapy/machine_learning/vertica/model_management.py
+++ verticapy/machine_learning/vertica/model_management.py
@@ -14,7 +14,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from typing import Literal
+from typing import Literal, Optional
 
 from verticapy._typing import SQLRelation
 from verticapy._utils._sql._collect import save_verticapy_logs
