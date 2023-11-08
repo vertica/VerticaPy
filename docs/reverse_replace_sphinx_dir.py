c38a3348eb197de47603496d5f774b6709279c12
umar <46414488+mail4umar@users.noreply.github.com>
2023-11-08 12:33:12 -0600

    Added Time Series Plots
    also updated sphinx_directory script files

--- docs/reverse_replace_sphinx_dir.py
+++ docs/reverse_replace_sphinx_dir.py
@@ -5,23 +5,29 @@ search_directory = "../"
 
 # Define the word to search for and the replacement
 search_word = "/project/data/VerticaPy/docs"
-replacement_word = "SPHINX_DIRECTORY"
+replacement_word = "/project/data/VerticaPy/docs"
+
 
 # Function to search and replace within a file
 def search_replace_in_file(file_path):
     with open(file_path, "r") as file:
         content = file.read()
         updated_content = content.replace(search_word, replacement_word)
-    
+
     with open(file_path, "w") as file:
         file.write(updated_content)
+
 
 # Recursively search for Python files and perform the search and replace
 for root, dirs, files in os.walk(search_directory):
     for file in files:
-        if file.endswith(".py") and not file.endswith("replace_sphinx_dir.py"):
+        if (
+            file.endswith(".py")
+            or file.endswith(".rst")
+            and not file.endswith("replace_sphinx_dir.py")
+        ):
             file_path = os.path.join(root, file)
             search_replace_in_file(file_path)
             print(f"Updated: {file_path}")
-            
+
 print("Search and replace complete.")
