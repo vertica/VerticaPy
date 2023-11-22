1c65fa891579cdf393a9a3b363d882e91997b53e
Badr Ouali <32390048+oualib@users.noreply.github.com>
2023-11-22 18:44:15 -0500

    Removing old plot tests (#901)
    * Removing old plot tests
    
    * fixed tests by switching default plotting library
    
    * black
    
    ---------
    
    Co-authored-by: Umar Farooq Ghumman <umarfarooq.ghumman@vertica.com>

--- verticapy/tests_new/machine_learning/vertica/conftest.py
+++ verticapy/tests_new/machine_learning/vertica/conftest.py
@@ -31,6 +31,7 @@ import xgboost as xgb
 from statsmodels.tsa.ar_model import AutoReg
 from statsmodels.tsa.arima.model import ARIMA
 from scipy.stats import f
+import verticapy as vp
 import verticapy.machine_learning.vertica as vpy_linear_model
 import verticapy.machine_learning.vertica.svm as vpy_svm
 import verticapy.machine_learning.vertica.tree as vpy_tree
@@ -45,6 +46,11 @@ if sys.version_info < (3, 12):
     import tensorflow as tf
 
 le = LabelEncoder()
+
+
+@pytest.fixture(autouse=True)
+def set_plotting_lib():
+    vp.set_option("plotting_lib", "matplotlib")
 
 
 @pytest.fixture(name="get_vpy_model", scope="function")
