98052e91012669e42377b7e9b1b9614849696d5e
Abhishek Sharma <137197172+abhsharma2@users.noreply.github.com>
2023-12-02 00:14:07 +0530

    Sphinx docstring changes for parser/pandas (#968)
    * Sphinx docstring changes for parser/pandas
    
    * corrections
    
    ---------
    
    Co-authored-by: Badr <badr.ouali@outlook.fr>

--- verticapy/core/parsers/pandas.py
+++ verticapy/core/parsers/pandas.py
@@ -43,55 +43,266 @@ def read_pandas(
     insert: bool = False,
 ) -> vDataFrame:
     """
-    Ingests a pandas DataFrame into the Vertica
-    database  by  creating a CSV file and  then
-    using flex tables to load the data.
+    Ingests a ``pandas.DataFrame`` into
+    the Vertica database by creating
+    a CSV file and then using flex
+    tables to load the data.
 
     Parameters
     ----------
     df: pandas.DataFrame
-        The pandas.DataFrame to ingest.
+        The ``pandas.DataFrame`` to
+        ingest.
     name: str, optional
-        Name  of  the new  relation or the  relation
-        in which to insert the data. If unspecified,
-        a  temporary  local  table is created.  This
-        temporary table is dropped at the end of the
-        local session.
+        Name of the new relation or
+        the relation in which to
+        insert the data.
+        If unspecified, a temporary
+        local table is created. This
+        temporary table is dropped at
+        the end of the local session.
     schema: str, optional
-        Schema  of  the new  relation.  If  empty, a
-        temporary  schema  is  used.  To modify  the
-        temporary   schema,  use  the   'set_option'
+        Schema of the new relation.
+        If empty, a temporary schema
+        is used. To modify the temporary
+        schema, use the :py:func:`set_option`
         function.
     dtype: dict, optional
-        Dictionary   of  input  types.  Providing  a
-        dictionary can increase  ingestion speed and
-        precision. If specified, rather than parsing
-        the intermediate CSV and  guessing the input
-        types,  VerticaPy uses  the specified  input
+        Dictionary of input types.
+        Providing a dictionary can
+        increase ingestion speed and
+        precision. If specified,
+        rather than parsing the
+        intermediate CSV and guessing
+        the input types, VerticaPy
+        uses the specified input
         types instead.
     parse_nrows: int, optional
-        If    this parameter  is  greater  than  zero,
-        VerticaPy  creates  and  ingests  a  temporary
-        file containing  'parse_nrows'  number of rows
-        to  determine  the  input  data types   before
-        ingesting the intermediate CSV file containing
-        the rest of the data. This method of data type
-        identification  is less accurate, but is  much
+        If this parameter is greater
+        than zero, VerticaPy creates
+        and ingests a temporary file
+        containing ``parse_nrows``
+        number of rows to determine
+        the input data types before
+        ingesting the intermediate
+        CSV file containing the rest
+        of the data. This method of
+        data type identification is
+        less accurate, but is much
         faster for large datasets.
     temp_path: str, optional
-        The path to which to write the intermediate CSV
-        file.  This is  useful in cases where the  user
-        does not have write  permissions on the current
-        directory.
+        The path to which to write
+        the intermediate CSV file.
+        This is useful in cases
+        where the user does not
+        have write permissions
+        on the current directory.
     insert: bool, optional
-        If set to True, the data are ingested into the
-        input relation. The column names of your table
-        and the pandas.DataFrame must match.
+        If set to ``True``, the
+        data are ingested into the
+        input relation. The column
+        names of your table and the
+        ``pandas.DataFrame`` must
+        match.
 
     Returns
     -------
     vDataFrame
-        vDataFrame of the new relation.
+        :py:class:`vDataFrame`
+        of the new relation.
+
+    Examples
+    ---------
+
+    In this example, we will first create
+    a ``pandas.DataFrame`` using
+    :py:meth:`verticapy.vDataFrame.to_pandas`
+    and ingest it into Vertica database.
+
+    We import :py:mod:`verticapy`:
+
+    .. ipython:: python
+
+        import verticapy as vp
+
+    .. hint::
+
+        By assigning an alias to :py:mod:`verticapy`,
+        we mitigate the risk of code collisions with
+        other libraries. This precaution is necessary
+        because verticapy uses commonly known function
+        names like "average" and "median", which can
+        potentially lead to naming conflicts. The use
+        of an alias ensures that the functions from
+        :py:mod:`verticapy` are used as intended
+        without interfering with functions from other
+        libraries.
+
+    We will use the Titanic dataset.
+
+    .. code-block:: python
+
+        import verticapy.datasets as vpd
+
+        data = vpd.load_titanic()
+
+    .. raw:: html
+        :file: SPHINX_DIRECTORY/figures/datasets_loaders_load_titanic.html
+
+    .. note::
+
+        VerticaPy offers a wide range of sample
+        datasets that are ideal for training
+        and testing purposes. You can explore
+        the full list of available datasets in
+        the :ref:`api.datasets`, which provides
+        detailed information on each dataset and
+        how to use them effectively. These datasets
+        are invaluable resources for honing your
+        data analysis and machine learning skills
+        within the VerticaPy environment.
+
+    .. ipython:: python
+        :suppress:
+
+        import verticapy.datasets as vpd
+
+        data = vpd.load_titanic()
+
+    Let's convert the :py:class:`vDataFrame`
+    to a ``pandas.DataFrame``.
+
+    .. code-block:: python
+
+        pandas_df = data.to_pandas()
+        display(pandas_df)
+
+    .. ipython:: python
+        :suppress:
+
+        pandas_df = data.to_pandas()
+        res = pandas_df
+        html_file = open("figures/core_parsers_pandas_1.html", "w")
+        html_file.write(res.to_html(max_rows = 6, justify = "center"))
+        html_file.close()
+
+    .. raw:: html
+        :file: SPHINX_DIRECTORY/figures/core_parsers_pandas_1.html
+
+    Now, we will ingest the
+    ``pandas.DataFrame``
+    into the Vertica database.
+
+    .. code-block:: python
+
+        from verticapy.core.parsers import read_pandas
+
+        read_pandas(
+            df = pandas_df,
+            name = "titanic_pandas",
+            schema = "public",
+        )
+
+    .. ipython:: python
+        :suppress:
+        :okexcept:
+
+        from verticapy.core.parsers import read_pandas
+        res = read_pandas(
+            df = pandas_df,
+            name = "titanic_pandas",
+            schema = "public",
+        )
+        html_file = open("figures/core_parsers_pandas_2.html", "w")
+        html_file.write(res._repr_html_())
+        html_file.close()
+
+    .. raw:: html
+        :file: SPHINX_DIRECTORY/figures/core_parsers_pandas_2.html
+
+    Let's specify data types using
+    "dtypes" parameter.
+
+    .. code-block:: python
+
+        read_pandas(
+            df = pandas_df,
+            name = "titanic_pandas_dtypes",
+            schema = "public",
+            dtype = {
+                "pclass": "Integer",
+                "survived": "Integer",
+                "name": "Varchar(164)",
+                "sex": "Varchar(20)",
+                "age": "Numeric(6,3)",
+                "sibsp": "Integer",
+                "parch": "Integer",
+                "ticket": "Varchar(36)",
+                "fare": "Numeric(10,5)",
+                "cabin": "Varchar(30)",
+                "embarked": "Varchar(20)",
+                "boat": "Varchar(100)",
+                "body": "Integer",
+                "home.dest": "Varchar(100)",
+            },
+        )
+
+    .. ipython:: python
+        :suppress:
+        :okexcept:
+
+        res = read_pandas(
+            df = pandas_df,
+            name = "titanic_pandas_dtypes",
+            schema = "public",
+            dtype = {
+                "pclass": "Integer",
+                "survived": "Integer",
+                "name": "Varchar(164)",
+                "sex": "Varchar(20)",
+                "age": "Numeric(6,3)",
+                "sibsp": "Integer",
+                "parch": "Integer",
+                "ticket": "Varchar(36)",
+                "fare": "Numeric(10,5)",
+                "cabin": "Varchar(30)",
+                "embarked": "Varchar(20)",
+                "boat": "Varchar(100)",
+                "body": "Integer",
+                "home.dest": "Varchar(100)",
+            },
+        )
+        html_file = open("figures/core_parsers_pandas_3.html", "w")
+        html_file.write(res._repr_html_())
+        html_file.close()
+
+    .. raw:: html
+        :file: SPHINX_DIRECTORY/figures/core_parsers_pandas_3.html
+
+    .. important::
+
+        A limited number of rows, determined by the
+        ``parse_nrows`` parameter, is ingested. If
+        your dataset is large and you want to ingest
+        the entire dataset, increase its value.
+
+    .. note::
+
+        During the ingestion process, an intermediate
+        CSV file is created. You can retrieve its
+        location by using the temp_path parameter.
+
+    .. note::
+
+        If you want to ingest into an existing table,
+        set the insert parameter to ``True``.
+
+    .. seealso::
+
+        | :py:meth:`verticapy.utilities.read_csv` :
+            Ingests a CSV file into the Vertica DB.
+        | :py:meth:`verticapy.utilities.read_json` :
+            Ingests a JSON file into the Vertica DB.
     """
     dtype = format_type(dtype, dtype=dict)
     if not schema:
