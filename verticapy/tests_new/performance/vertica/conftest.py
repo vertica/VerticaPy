d870d589873d9f92ba55f4a38eadc6fa11f45881
Jason Slaunwhite <jslaunwhite@opentext.com>
2023-12-11 18:34:57 -0500

    VER-90918: Connection from environment

--- verticapy/tests_new/performance/vertica/conftest.py
+++ verticapy/tests_new/performance/vertica/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import os
 
 # verticapy is notebook integration
 import verticapy as vp
@@ -19,7 +20,7 @@ def vp_connect_get_name():
 
 @pytest.fixture
 def raw_client_cursor():
-    info_map = get_connection_info()
+    info_map = get_connection_info(verbose=False)
     conn = None
     try:
         conn = vertica_python.connect(**info_map)
@@ -28,12 +29,41 @@ def raw_client_cursor():
         if conn is not None:
             conn.close()
 
-def get_connection_info():
-    # TODO: hard-coded today
-    # TODO: Should read from environment veriables
-    return {"host": "engdev4.verticacorp.com",
-            "port": "32788",
-            "database": "jslaunwhite",
-            "password": "$vertica$",
-            "user": "jslaunwhite",
+def get_connection_info(verbose=True):
+    # Fetch from the environment
+    host = os.environ.get("VP_TEST_HOST", None)
+    db = os.environ.get("VP_TEST_DATABASE", None)
+    port = os.environ.get("VP_TEST_PORT", "5433")
+    password = os.environ.get("VP_TEST_PASSWORD", None)
+
+    _throw_if_none(host, "VP_TEST_HOST")
+    _throw_if_none(db, "VP_TEST_DATABASE")
+
+    # User is also required, but it could come from
+    # two sources
+    test_user = os.environ.get("VP_TEST_USER", None)
+    user = test_user if test_user is not None else os.environ.get("USER", None)
+
+    if user is None:
+        raise ValueError("Neither VP_TEST_USER nor USER are set, but the test framework expected them to be set")
+    msg = ("\nConnection info:\n"
+           f"host = {host}\n"
+           f"port = {port}\n"
+           f"database = {db}\n"
+           f"user = {user}\n"
+           f"password = {'EMPTY' if password is None else '(REDACTED)'}\n")
+    if (verbose):
+        print(msg)
+    logging.info(msg)
+
+    return {"host": host,
+            "port": port,
+            "database": db,
+            "password": password,
+            "user": user,
             "connection_timeout": 5}
+
+def _throw_if_none(value, env_var):
+    if value is None:
+        raise ValueError(f"{env_var} was not set, but the test framework expected it to be set")
+
