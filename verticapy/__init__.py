c03acab049299bc7e1126db4cd2d75bc6d592ec9
Badr Ouali <32390048+oualib@users.noreply.github.com>
2023-12-11 20:42:55 -0500

    QPROF Improvements (#1033)
    * QPROF Improvements
    
     - tree legend + root
     - graph display correction.
     - customize parameters.
    
    * Update __init__.py

--- verticapy/__init__.py
+++ verticapy/__init__.py
@@ -37,9 +37,9 @@ __description__: str = (
 __url__: str = "https://github.com/vertica/verticapy/"
 __license__: str = "Apache License, Version 2.0"
 __version__: str = "1.0.0"
-__iteration__: int = 2
+__iteration__: int = 3
 __date__: str = "12112023"
-__last_commit___: str = "65142180dc771cad6625dc7bcd1674b7ee2cc86d"
+__last_commit___: str = "7e0dd07c53b729a067b249402ed465d45d4d15df"
 __long_version__: str = f"{__version__}-{__iteration__}—{__date__}-{__last_commit___}"
 __codecov__: float = 0.8316
 
