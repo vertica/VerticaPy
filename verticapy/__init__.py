b2b91cee525fa71093f4958f2b7bf92e8f6a64a1
Badr Ouali <32390048+oualib@users.noreply.github.com>
2023-12-21 16:23:02 -0500

    Qprof integration v2 00 (#1064)
    * [QPROF] Improvement - Metrics
    
     - Correcting a bug.
     - Adding the possibility to pick up a metric.
    
    [TODO]
    The possibility to work with two metrics.
    
    * bug corrections
    
    * Update __init__.py

--- verticapy/__init__.py
+++ verticapy/__init__.py
@@ -37,9 +37,9 @@ __description__: str = (
 __url__: str = "https://github.com/vertica/verticapy/"
 __license__: str = "Apache License, Version 2.0"
 __version__: str = "1.0.0"
-__iteration__: int = 1
+__iteration__: int = 2
 __date__: str = "12212023"
-__last_commit__: str = "b1181fad2e7ed1a946c49f2d3b843145b80ea0fb"
+__last_commit__: str = "3283cf35960357102329ed6b2a15e8902afb3306"
 __long_version__: str = f"{__version__}-{__iteration__}—{__date__}-{__last_commit__}"
 __codecov__: float = 0.8316
 
