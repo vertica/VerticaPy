3bb748a60eb7d8a87f10ef6c8d959dfc5059e28f
Badr <badr.ouali@outlook.fr>
2024-01-03 17:28:36 -0500

    Update __init__.py

--- verticapy/__init__.py
+++ verticapy/__init__.py
@@ -37,9 +37,9 @@ __description__: str = (
 __url__: str = "https://github.com/vertica/verticapy/"
 __license__: str = "Apache License, Version 2.0"
 __version__: str = "1.0.0"
-__iteration__: int = 2
-__date__: str = "01012024"
-__last_commit__: str = "b751fd9ef9d312846b54d892ac0b9fa3c2e937c5"
+__iteration__: int = 1
+__date__: str = "01032024"
+__last_commit__: str = "a06640a3cc5a69a826c00f030c49fd89905827b8"
 __long_version__: str = f"{__version__}-{__iteration__}—{__date__}-{__last_commit__}"
 __codecov__: float = 0.8316
 
