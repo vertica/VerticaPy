d42e6c0262af4bd33b5791ae8dde1ab692c230ac
roypaulin <roy.paulin@microfocus.com>
2024-01-23 21:45:06 +0100

    Create QueryProfilerInterface

--- verticapy/jupyter/html/__init__.py
+++ verticapy/jupyter/html/__init__.py
@@ -14,5 +14,3 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 See the  License for the specific  language governing
 permissions and limitations under the License.
 """
-from verticapy.performance.vertica.qprof import QueryProfiler
-from verticapy.performance.vertica.qprof_interface import QueryProfilerInterface
