efc6f3e8e0da15ad24740d56e92ae9ebbab619d4
Badr <badr.ouali@outlook.fr>
2024-04-25 09:13:06 -0400

    adding import

--- verticapy/machine_learning/vertica/__init__.py
+++ verticapy/machine_learning/vertica/__init__.py
@@ -75,4 +75,4 @@ from verticapy.machine_learning.vertica.tree import (
     DummyTreeClassifier,
     DummyTreeRegressor,
 )
-from verticapy.machine_learning.vertica.tsa import ARIMA, ARMA, AR, MA
+from verticapy.machine_learning.vertica.tsa import ARIMA, ARMA, AR, MA, VAR
