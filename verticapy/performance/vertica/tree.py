8ca3c10cc479cb3da4ff3eb1bbe3a22627f5638a
Badr <badr.ouali@outlook.fr>
2024-09-26 10:45:44 -0400

    [QPROF] Many enhancements
    Possibility to filter based on tooltip.
    Should close:
     - https://jira.verticacorp.com/jira/browse/VER-97128?filter=-1
     - https://jira.verticacorp.com/jira/browse/VER-97129?filter=-1

--- verticapy/performance/vertica/tree.py
+++ verticapy/performance/vertica/tree.py
@@ -353,6 +353,8 @@ class PerformanceTree:
             d["threshold_metric2"] = None
         if "op_filter" not in d:
             d["op_filter"] = None
+        if "tooltip_filter" not in d:
+            d["tooltip_filter"] = None
         if "display_path_transition" not in d:
             d["display_path_transition"] = True
         if "display_annotations" not in d:
@@ -762,7 +764,7 @@ class PerformanceTree:
         for more information.
         """
         op_filter = self.style["op_filter"]
-        if not (op_filter):
+        if not(op_filter):
             return True
         path_id_op = self._get_operators_path_id(path_id)
         path_id_op = [str(op).lower().strip() for op in path_id_op]
@@ -1528,6 +1530,7 @@ class PerformanceTree:
         colors: list,
         operator: Optional[str] = None,
         legend_metrics: Optional[list] = None,
+        tooptip_description: Optional[str] = None,
     ) -> str:
         """
         Generates the Graphviz
@@ -1537,13 +1540,17 @@ class PerformanceTree:
 
         Parameters
         ----------
-        label: int / str
+        label: int | str
             The node label.
         colors: list
             A ``list`` of one or
             two colors.
         operator: str, optional
             Operator Icon.
+        legend_metrics: list, optional
+            Metrics values.
+        tooptip_description: str, optional
+            Tooltip Description.
 
         Returns
         -------
@@ -1592,10 +1599,20 @@ class PerformanceTree:
                 display_path_id = False
 
         # Filter based on the operator.
-        filter_op = self._is_op_in_path_id(label)
+        filter_op = not(self._is_op_in_path_id(label))
+        if filter_op:
+            display_path_id = False
+
+        # Filter based on the tooptip.
+        tooptip_description = str(tooptip_description).lower().strip()
+        if not(isinstance(self.style["tooltip_filter"], NoneType)):
+            tooltip_filter = str(self.style["tooltip_filter"]).lower().strip()
+            filter_tooltip = tooltip_filter not in tooptip_description
+            if filter_tooltip:
+                display_path_id = False
 
         # Special Display.
-        if not (display_path_id) or not (filter_op):
+        if not (display_path_id):
             return (
                 '<<TABLE border="1" cellborder="1" cellspacing="0" '
                 f'cellpadding="0"><TR><TD WIDTH="{width * 2}" '
@@ -1846,6 +1863,7 @@ class PerformanceTree:
                 colors,
                 operator=row,
                 legend_metrics=legend_metrics,
+                tooptip_description=row,
             )
 
             if tree_id in links and display_tr:
@@ -1858,7 +1876,7 @@ class PerformanceTree:
                 if ns_icon != "":
                     ns_icon += " "
                 ns_icon += QprofUtility._get_execute_on(tooltip)
-                if not (display_path_id) or not (self._is_op_in_path_id(label)):
+                if not (display_path_id) or not(self._is_op_in_path_id(label)):
                     ns_icon = ""
                 # Final Tooltip.
                 description = "\n\nDescriptors\n------------\n" + "\n".join(
